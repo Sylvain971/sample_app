ed3d501f49fa6924d0b6967280396fa3865505b0
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-02-10 23:04:13 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
 
-  get 'foo/baz'
+  root 'static_pages#home'
 
   get 'static_pages/home'
 
   get 'static_pages/help'
 
- root 'application#hello'
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
 end
